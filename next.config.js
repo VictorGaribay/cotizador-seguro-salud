45a59eba8f41f99f2f0283e5b48c871defe1bf8c
victor garibay <victor.garibayy@gmail.com>
2021-01-31 04:50:32 -0500

    ADD: dotenv and axios

--- next.config.js
+++ next.config.js
@@ -1,8 +1,15 @@
 //pemrmitira importa > import image from 'url'
 const withSass = require('@zeit/next-sass')
 const withCSS = require('@zeit/next-css')
+const dotEnvResult = require('dotenv').config()
+if (dotEnvResult.error) {
+  throw dotEnvResult.error
+}
 module.exports = withCSS(
   withSass({
+    env: {
+      BASE_URL: process.env.BASE_URL
+    },
     webpack(config, options) {
       config.module.rules.push({
         test: /\.(png|jpg|gif|svg|eot|ttf|woff|woff2)$/,
